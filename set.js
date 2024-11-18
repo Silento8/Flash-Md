6975beaa3aaff954c481ac1c43fd77633ca1a73d
Silento8 <165663375+Silento8@users.noreply.github.com>
2024-11-18 23:26:12 +0300

    Update set.js

--- set.js
+++ set.js
@@ -11,12 +11,12 @@ const databasePath = path.join(__dirname, './database.db');
 const DATABASE_URL = process.env.DATABASE_URL === undefined ? databasePath : process.env.DATABASE_URL;
 
 module.exports = {
-    session: process.env.SESSION_ID || 'FLASH-MD-WA-BOT;;;=>',
+    session: process.env.SESSION_ID || 'FLASH-MD-WA-BOT;;;=>eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiVUNrdDlmMkkxVVljeTZPUjB4ZUpIbG91aHRPT1UrK0F2anBOMDJKa1duUT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiV1QzSUx0clZBeHlveWptY1FicGlpMFhNU1hiK2FrVnovSjNTNXNDbHhTMD0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiIrRU82NmZKNER0RTlZN0Q5K0NvdXJxcUhnaFhiZlZMRDNjWE8vOVlxem1rPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJBN0JVYzNDcG1nSm0wV3RzczVHVVlxbUxVYWhLZDRkUVB1Z0paUXdDNVhBPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjhCYWV1aC8rQzZjUzZxejFkckVIdWZkVEhoclpOVmtsM0MwaFpGdHZxV2c9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlV1cktOUSt5cCtYV2UrRitzb0d1TU53YnUrYXBGLzJFcDlxMFVrSnpReTA9In19LCJzaWduZWRQ…;;;=>',
     PREFIXES: (process.env.PREFIX || '').split(',').map(prefix => prefix.trim()).filter(Boolean),
-    OWNER_NAME: process.env.OWNER_NAME || "France King",
-    OWNER_NUMBER: process.env.OWNER_NUMBER || "254105915061",
-    AUTO_READ_STATUS: process.env.AUTO_VIEW_STATUS || "on",
-    AUTOREAD_MESSAGES: process.env.AUTO_READ_MESSAGES || "on",
+    OWNER_NAME: process.env.OWNER_NAME || "Alexking",
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "254718387818",
+    AUTO_READ_STATUS: process.env.AUTO_VIEW_STATUS || "off",
+    AUTOREAD_MESSAGES: process.env.AUTO_READ_MESSAGES || "off",
     CHATBOT: process.env.CHAT_BOT || "off",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_SAVE_STATUS || 'off',
     A_REACT: process.env.AUTO_REACTION || 'on',
@@ -32,7 +32,7 @@ module.exports = {
     ADM: process.env.ANTI_DELETE || 'on',
     TZ: process.env.TIME_ZONE || 'Africa/Nairobi',
     DP: process.env.STARTING_MESSAGE || "on",
-    ANTICALL: process.env.ANTICALL || 'on',
+    ANTICALL: process.env.ANTICALL || 'off',
     DATABASE_URL,
     DATABASE: DATABASE_URL === databasePath
         ? "postgresql://flashmd_user:JlUe2Vs0UuBGh0sXz7rxONTeXSOra9XP@dpg-cqbd04tumphs73d2706g-a/flashmd"
